1973706f013f9023d3baf3ce3359962e33e3f956
Ben Sirivallop <bsirival@uci.edu>
2024-10-21 19:26:49 -0700

    created wait function to work with robot.txt guidelines

--- driver.py
+++ driver.py
@@ -1,7 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
+
+
+def wait(secs:int):
+    time.sleep(secs)
+
 
 driver = webdriver.Chrome()
 driver.get("https://calendar.library.ucsc.edu/spaces")
+wait(2)
+
 
 
