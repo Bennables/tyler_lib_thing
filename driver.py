07b08fc7585aa0d4802b8d6a02ac62e8cbb9544e
Ben <bsirival@uci.edu>
2024-10-22 10:55:24 -0700

    Sorts correctly now

--- driver.py
+++ driver.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import datetime
-import re
 
 
 def wait(secs:int):
@@ -14,10 +13,10 @@ driver.get("https://calendar.library.ucsc.edu/spaces")
 wait(10)
 
 available = driver.find_elements(by = By.CLASS_NAME, value = "s-lc-eq-avail")
-for i in available:
-    print(i.get_attribute("title").split(' ')[0])
+# for i in available:
+    # print(i.get_attribute("title").split(' ')[0])
 
-available = sorted(available, key = lambda index : ( datetime.datetime.strptime(index.get_attribute("title").split(' ')[0], "%I:%M%p"), index.get_attribute("title")[13:]))
+available = sorted(available, key = lambda index : (index.get_attribute("title")[index.get_attribute('title').index('-') + 2:], datetime.datetime.strptime(index.get_attribute("title").split(' ')[0], "%I:%M%p")))
 for i in available:
     print(i.get_attribute("title"))
 print(available)
