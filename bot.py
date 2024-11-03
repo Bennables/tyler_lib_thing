d344ef950420ec06e07bbfbedfe7c8199519747f
Ben <bsirival@uci.edu>
2024-11-02 20:26:46 -0700

    making sure users dont have duplicate profiles.

--- bot.py
+++ bot.py
@@ -19,28 +19,33 @@ async def book(ctx, time):
 async def add_prof(ctx, *args):
     #getting the arguments
     #check if we can add them
-    if len(args) == 3:
+    args = list(args)
+    args.insert(0, ctx.author.name)
+    delete_row = None
+    if len(args) == 4:
         with open('accounts.csv', 'a', newline = '') as file:
+            csv_reader = csv.DictReader(file)
+            current_people = [row["ColumnName"] for row in csv_reader]
+            #remove row and clean data
+            if args[0] in current_people:
+                delete_row = args[0]
+            with open("accounts.csv", mode="r", newline="") as infile:
+                reader = csv.reader(infile)
+                rows = [row for row in reader if any(row) or row['Name'] == delete_row]  # Keep only non-empty rows
+
+            # Write the cleaned data back to the same file
+            with open("accounts.csv", mode="w", newline="") as outfile:
+                writer = csv.writer(outfile)
+                writer.writerows(rows)
+            
             writer = csv.writer(file)
-            writer.writerow(list(args))
+            writer.writerow(args)
             await ctx.send(f"UPDATED!")
         print("DONE")
     else:
         #send the correct format
         await ctx.send(f"Enter your first name, last name, email") 
         await ctx.send(f'Format: "\!add_prof first last email"')
-
-    #Cleaning empty lines
-    # Read the original file and filter out empty lines
-    with open("accounts.csv", mode="r", newline="") as infile:
-        reader = csv.reader(infile)
-        rows = [row for row in reader if any(row)]  # Keep only non-empty rows
-
-    # Write the cleaned data back to the same file
-    with open("accounts.csv", mode="w", newline="") as outfile:
-        writer = csv.writer(outfile)
-        writer.writerows(rows)
-
 
 # Run the bot with your token
 bot.run(TOKEN)
